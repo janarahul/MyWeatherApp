832e308355844fc53dc99b73058ab03e0b7f5395
janarahul <janarahul123@outlook.com>
2017-09-23 10:14:47 +0530

    UI updated and Features Added

--- app/src/main/java/com/example/rahul/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/rahul/myweatherapp/MainActivity.java
@@ -4,25 +4,35 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+    String main,description;
+    TextView textView;
+    EditText editText;
     private class DownloadJSON extends AsyncTask<String, Void,String> {
 
         @Override
         protected String doInBackground(String... urls) {
             try {
                 URL url = new URL(urls[0]);
-                Log.i("before","eeeee");
+
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.connect();
-                Log.i("after","eeeee");
+
                 InputStream is = connection.getInputStream();
                 InputStreamReader isr = new InputStreamReader(is);
                 int data ;
@@ -32,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                     result += (char)data;
                     data = isr.read();
                 }
+                result = result.replace("null","");
                 return result;
             } catch (IOException e) {
                 e.printStackTrace();
@@ -43,7 +54,32 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            Log.i("weatherinner",result);
+            try {
+                JSONObject resultJSON = new JSONObject(result);
+                String weather = resultJSON.getString("weather");
+                Log.i("weather",weather);
+
+                JSONArray jsonArray = new JSONArray(weather);
+
+                for(int i=0;i<jsonArray.length();i++){
+                    JSONObject jsonPart = jsonArray.getJSONObject(i);
+                    textView.append(jsonPart.getString("main")+"\n");
+                    //textView.append(jsonPart.getString("description")+"\n");
+                }
+
+                String metrics = resultJSON.getString("main");
+                Log.i("metricmain",metrics);
+
+                JSONObject jsonObject = new JSONObject(metrics);
+
+
+                    textView.append("Temperature: "+jsonObject.getString("temp")+"°C\n");
+                    textView.append("Humidity: "+jsonObject.getString("humidity")+"\n");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
@@ -51,16 +87,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText)findViewById(R.id.editText);
+        textView =(TextView)findViewById(R.id.textView);
 
-        DownloadJSON dj = new DownloadJSON();
-        try {
-            String json = dj.execute("http://api.openweathermap.org/data/2.5/weather?q=Bengaluru&appid=ba9b478ac03c34c89c858c6e03f66077").get();
-            Log.i("weather",json);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                        actionId == EditorInfo.IME_ACTION_DONE ||
+                        event.getAction() == KeyEvent.ACTION_DOWN &&
+                                event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    textView.setText("");
+                    DownloadJSON dj = new DownloadJSON();
+                    dj.execute("http://api.openweathermap.org/data/2.5/weather?q="+editText.getText()+"&units=metric&appid=ba9b478ac03c34c89c858c6e03f66077");
+
+                    return true;
+
+                }
+                return false; // pass on to other listeners.
+            }
+        });
+
+
+
 
     }
 }
